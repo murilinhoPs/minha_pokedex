68fd50f0e8c74996d8bb694bd89ff44cd9457eb9
MurilinhoPs <murilinhorps@gmail.com>
2021-12-07 18:46:34 -0300

    Change background color and add GetAllPokemonsUsecase to test it

--- lib/src/application/pages/home.dart
+++ lib/src/application/pages/home.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:minha_pokedex/src/domain/use_cases/fav_pokemons/get_all_pokemons_use_case.dart';
 
 class MyHomePage extends StatelessWidget {
+  final getAllPokemons = GetIt.I.get<GetAllPokemonsUsecase>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       appBar: AppBar(
         title: Text('Pokédex'),
       ),
       body: Center(
         child: Text(
           'Minha futura pokédex',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          getAllPokemons();
+        },
         tooltip: 'favorite',
-        child: Icon(Icons.favorite),
+        child: Icon(
+          Icons.favorite,
+          color: Colors.grey[800],
+        ),
       ),
     );
   }
