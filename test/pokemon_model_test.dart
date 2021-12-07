780bbc04ebda953da3dabda308db5247c8a9f938
MurilinhoPs <murilinhorps@gmail.com>
2021-12-07 18:46:45 -0300

    Create tests to PokemonModel

---
+++ test/pokemon_model_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:minha_pokedex/src/infra/models/pokemon_model.dart';
+
+void main() {
+  test('Default test', () {
+    final model = PokemonModel(
+      id: 1,
+      name: 'pokemon',
+      imageUrl: 'nada',
+      pokedexNumber: 000,
+      types: ['grass', 'dark'],
+    );
+
+    final jsonModel = model.toJson();
+    print(jsonModel);
+
+    final fromJsonModel = PokemonModel.fromJson(jsonModel);
+    print(fromJsonModel.toString());
+
+    expect(model.types, fromJsonModel.types);
+  });
+}
