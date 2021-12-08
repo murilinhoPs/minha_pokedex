74ddf05a87fd146ae8d66054bf3f816d6aa85387
MurilinhoPs <murilinhorps@gmail.com>
2021-12-08 19:16:52 -0300

    Rename to getAllFavPokemons

--- lib/src/domain/use_cases/fav_pokemons/get_all_fav_pokemons_use_case.dart
+++ lib/src/domain/use_cases/fav_pokemons/get_all_fav_pokemons_use_case.dart
@@ -2,10 +2,10 @@ import 'package:minha_pokedex/src/domain/entities/pokemon.dart';
 import 'package:minha_pokedex/src/domain/exceptions/pokemon_storage_exceptions.dart';
 import 'package:minha_pokedex/src/domain/repositories/fav_pokemons_storage_repository.dart';
 
-class GetAllPokemonsUsecase {
+class GetAllFavPokemonsUsecase {
   final FavPokemonsStorageRepository favPokemonsStorageRepository;
 
-  GetAllPokemonsUsecase({
+  GetAllFavPokemonsUsecase({
     required this.favPokemonsStorageRepository,
   });
 
