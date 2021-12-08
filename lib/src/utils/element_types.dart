0370e8e193da491d0e2f12ce3bbffa1791a79f25
MurilinhoPs <murilinhorps@gmail.com>
2021-12-08 19:17:27 -0300

    Create elementTypes in utils/ to create a map between pokmeon type an its color

---
+++ lib/src/utils/element_types.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/painting.dart';
+
+const elementTypes = {
+  'grass': Color(0xff78C850),
+  'poison': Color(0xffA040A0),
+  'fire': Color(0xffF08030),
+  'flying': Color(0xffA890F000),
+  'water': Color(0xff6890F0),
+  'bug': Color(0xff6D7815),
+  'normal': Color(0xffA8A878),
+  'electric': Color(0xffF8D030),
+  'ground': Color(0xffE0C068),
+  'fighting': Color(0xffC03028),
+  'psychic': Color(0xffF85888),
+  'rock': Color(0xffB8A038),
+  'ice': Color(0xff98D8D8),
+  'ghost': Color(0xff705898),
+  'dragon': Color(0xff4924A1),
+  'fairy': Color(0xffEE99AC),
+  'dark': Color(0xff705848),
+  'steel': Color(0xffB8B8D0),
+};
