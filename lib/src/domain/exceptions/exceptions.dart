59136afb74654ff6ca46c66723781474f4227782
MurilinhoPs <murilinhorps@gmail.com>
2021-12-08 10:26:34 -0300

    Add all custom exceptions from PokemonStorage

--- lib/src/domain/exceptions/exceptions.dart
+++ lib/src/domain/exceptions/exceptions.dart
@@ -4,5 +4,31 @@ abstract class PokemonsStorageExceptions implements Exception {
 }
 
 class CouldNotGetAllPokemons extends PokemonsStorageExceptions {
-  CouldNotGetAllPokemons(String message) : super(message);
+  CouldNotGetAllPokemons() : super('CouldNotGetAllPokemons');
+}
+
+class CouldNotGetPokemon extends PokemonsStorageExceptions {
+  CouldNotGetPokemon() : super('CouldNotGetPokemon with details');
+}
+
+class CouldNotAddFavPokemon extends PokemonsStorageExceptions {
+  CouldNotAddFavPokemon() : super('CouldNotAddFavPokemon to fav list');
+}
+
+class CouldNotUpdateFavPokemon extends PokemonsStorageExceptions {
+  CouldNotUpdateFavPokemon()
+      : super(
+          'CouldNotUpdateFavPokemon to add or remove from fav list',
+        );
+}
+
+class CouldNotDeletePokemonFromFav extends PokemonsStorageExceptions {
+  CouldNotDeletePokemonFromFav() : super('CouldNotDeletePokemonFromFav');
+}
+
+class CouldNotDeleteAllPokemons extends PokemonsStorageExceptions {
+  CouldNotDeleteAllPokemons()
+      : super(
+          'CouldNotDeleteAllPokemons from fav storage',
+        );
 }
