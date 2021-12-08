1df2ea7440bdadb6f751ef544589017345973dd2
MurilinhoPs <murilinhorps@gmail.com>
2021-12-08 19:21:30 -0300

    Testing usecases on home page

--- lib/src/application/pages/home.dart
+++ lib/src/application/pages/home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:minha_pokedex/src/domain/use_cases/fav_pokemons/get_all_pokemons_use_case.dart';
+import 'package:minha_pokedex/src/domain/use_cases/fav_pokemons/get_all_fav_pokemons_use_case.dart';
+import 'package:minha_pokedex/src/domain/use_cases/poke_api/get_pokemon_details_use_case.dart';
+import 'package:minha_pokedex/src/domain/use_cases/poke_api/get_pokemons_use_case.dart';
 
 class MyHomePage extends StatelessWidget {
-  final getAllPokemons = GetIt.I.get<GetAllPokemonsUsecase>();
+  final getFavPokemons = GetIt.I.get<GetAllFavPokemonsUsecase>();
+
+  final getPokemons = GetIt.I.get<GetPokemonsUseCase>();
+  final getPokemon = GetIt.I.get<GetPokemonDetailsUseCase>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +27,9 @@ class MyHomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          getAllPokemons();
+          // getAllPokemons();
+          getPokemons();
+          // getPokemon(1);
         },
         tooltip: 'favorite',
         child: Icon(
