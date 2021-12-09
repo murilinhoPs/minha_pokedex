e49afed1617dc6333a5dd85808a7bba19f3203d7
MurilinhoPs <murilinhorps@gmail.com>
2021-12-09 14:06:30 -0300

    Create application pages, with default values

---
+++ lib/src/application/pages/fav_pokemons/fav_pokemons_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavPokemonsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('FavPokemons'),
+    );
+  }
+}
---
+++ lib/src/application/pages/pokedex_pokemon_details/pokedex_pokemon_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PokedexPokemonDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Pokemon details from pokedex'),
+    );
+  }
+}
