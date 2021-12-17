19e6ae323ebb782718ce9c872dcb1e1383ce60dc
murilinhops <murilo.pescatori@nubank.com.br>
2021-12-17 18:01:04 -0300

    Add onLoadMore logic UI

--- lib/src/application/pages/pokedex_home/pokedex_home_page.dart
+++ lib/src/application/pages/pokedex_home/pokedex_home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -9,7 +10,6 @@ import 'package:minha_pokedex/src/application/pages/pokedex_home/widgets/pokedex
 import 'package:minha_pokedex/src/application/widgets/loading_indicator.dart';
 import 'package:minha_pokedex/src/application/widgets/pokemon_list.dart';
 import 'package:minha_pokedex/src/application/widgets/reload_content_button.dart';
-import 'package:minha_pokedex/src/domain/entities/pokemon.dart';
 import 'package:minha_pokedex/src/utils/assets.dart';
 import 'package:minha_pokedex/src/utils/strings.dart';
 
@@ -22,6 +22,11 @@ class _PokedexHomePageState extends State<PokedexHomePage> {
   final pokedexSearchBloc = GetIt.I.get<PokedexSearchBloc>();
   final scrollController = ScrollController();
 
+  bool get canLoadMore =>
+      scrollController.offset > scrollController.position.maxScrollExtent + 100;
+
+  bool showRefreshIndicator = false;
+
   @override
   void initState() {
     pokedexSearchBloc.add(
@@ -32,19 +37,24 @@ class _PokedexHomePageState extends State<PokedexHomePage> {
   }
 
   void _nextPageListener() {
-    if (scrollController.offset == scrollController.position.maxScrollExtent) {
-      Timer(Duration(milliseconds: 20), () {
+    if (canLoadMore) {
+      Timer(Duration(milliseconds: 30), () {
         scrollController.animateTo(
           scrollController.position.maxScrollExtent,
-          duration: Duration(milliseconds: 240),
-          curve: Curves.decelerate,
+          duration: Duration(milliseconds: 420),
+          curve: Curves.ease,
         );
       });
 
+      if (pokedexSearchBloc.state.status == SearchStatus.filterSuccess) {
+        pokedexSearchBloc.add(
+          PokedexSearchMorePokemonsFetched(),
+        );
+        return;
+      }
+
       pokedexSearchBloc.add(
-        PokedexSearchNextPageFetched(
-          pageOffset: pokedexSearchBloc.state.currentPageOffset,
-        ),
+        PokedexSearchNextPageFetched(),
       );
     }
   }
@@ -58,35 +68,32 @@ class _PokedexHomePageState extends State<PokedexHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              top: -182,
-              left: 0,
-              right: 0,
-              child: Container(
-                alignment: Alignment.topCenter,
-                child: Image.asset(
-                  Assets.pokeball3x,
-                  scale: 0.62,
-                  color: Colors.grey.withOpacity(0.2),
-                ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: -108,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: Image.asset(
+                Assets.pokeball3x,
+                scale: 0.68,
+                color: Colors.grey.withOpacity(0.24),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(
-                16.0,
-                12.0,
-                16.0,
-                0.0,
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+            ),
+            child: SafeArea(
               child: Column(
                 children: [
                   PokedexHeader(
                     onChanged: _onSearchPokemon,
                   ),
-                  SizedBox(height: 16.0),
+                  SizedBox(height: 20.0),
                   Expanded(
                     child: _buildPokemonCards(),
                   ),
@@ -94,12 +101,12 @@ class _PokedexHomePageState extends State<PokedexHomePage> {
                 ],
               ),
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: BottomNavBar(),
-            ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: BottomNavBar(),
+          ),
+        ],
       ),
     );
   }
@@ -130,6 +137,21 @@ class _PokedexHomePageState extends State<PokedexHomePage> {
           pokemons: pokemonList,
           controller: scrollController,
           isLoadingPokemons: status == SearchStatus.nextPageLoading,
+          canLoadMore: status == SearchStatus.filterSuccess,
+        );
+        Column(
+          children: [
+            Text('Total: ${state.pokemons.length}'),
+            SizedBox(height: 12),
+            Expanded(
+              child: PokemonList(
+                pokemons: pokemonList,
+                controller: scrollController,
+                isLoadingPokemons: status == SearchStatus.nextPageLoading,
+                canLoadMore: status == SearchStatus.filterSuccess,
+              ),
+            ),
+          ],
         );
       },
     );
